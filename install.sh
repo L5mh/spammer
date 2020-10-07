2cd449651d52616ffa2a48ff5d5051b53f6e5c4c
cludeex <cludeex@gmail.com>
2020-10-07 10:57:02 +0300

    INSTALL

--- install.sh
+++ install.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
-if [ -d "/data/data/com.termux/files/usr/" ]; then
-pkg install python dos2unix
-pip install requests colorama
-cp ~/spammer/spammer.py $PREFIX/bin/spammer
-dos2unix $PREFIX/bin/spammer
-chmod +x $PREFIX/bin/spammer
-spammer
-elif [ -d "/usr/bin/" ]; then
-sudo apt install python3 python3-pip dos2unix
-pip3 install requests colorama
-sudo cp ~/spammer/spammer.py $PREFIX/bin/spammer
-dos2unix $RPEFIX/bin/spammer
-chmod +x $RPEFIX/bin/spammer
-spammer
-else
-echo "spammer cannot be installed"
+if [ -d "/data/data/com.termux/files/usr/" ]
+then
+	apt update upgrade
+	pkg install python dos2unix
+	pip install requests colorama fake-useragent
+	cp ~/spammer/spammer.py $PREFIX/bin/spammer
+	dos2unix $PREFIX/bin/spammer
+	chmod +x $PREFIX/bin/spammer
+	spammer
+elif [ -d "/usr/bin/" ]
+then
+	sudo apt-get update upgrade
+	sudo apt-get install python3 python3-pip dos2unix
+	pip3 install requests colorama fake-useragent
+	sudo cp ~/spammer/spammer.py /usr/local/bin/spammer
+	dos2unix /usr/local/bin/spammer
+	chmod +x /usr/local/bin/spammer
+	spammer
 fi
