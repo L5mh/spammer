e33c9985ab514c733ca1030afb5e1f7aaadb5a9c
cludeex <cludeex@gmail.com>
2020-10-12 10:19:17 +0300

    INSTALL

--- install.sh
+++ install.sh
@@ -2,9 +2,9 @@
 clear
 if [ -d "/data/data/com.termux/files/usr/" ]
 then
-	apt update upgrade
-	pkg install python dos2unix
-	pip install requests colorama fake-useragent
+	apt update upgrade -y
+	pkg install python dos2unix -y
+	pip install -r requirements.txt
 	cp ~/spammer/spammer.py $PREFIX/bin/spammer
 	dos2unix $PREFIX/bin/spammer
 	chmod +x $PREFIX/bin/spammer
@@ -16,9 +16,9 @@ then
                         echo "Запустите install.sh с root правами"
                         exit
                 else
-                        apt-get update upgrade
-        		apt-get install python3 python3-pip dos2unix
-        		pip3 install requests colorama fake-useragent
+                        apt-get update upgrade -y
+        		apt-get install python3 python3-pip dos2unix -y
+        		pip3 install -r requirements.txt
         		cp ~/spammer/spammer.py /usr/local/bin/spammer
         		dos2unix /usr/local/bin/spammer
         		chmod +x /usr/local/bin/spammer
