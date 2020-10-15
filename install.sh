74ebe092e420f7c065c7132b3838e1cdc69f4557
cludeex <cludeex@gmail.com>
2020-10-15 10:48:07 +0300

    INSTALL

--- install.sh
+++ install.sh
@@ -1,27 +1,65 @@
 #!/bin/bash
 clear
-if [ -d "/data/data/com.termux/files/usr/" ]
-then
-	apt update upgrade -y
-	pkg install python dos2unix -y
-	pip install -r requirements.txt
-	cp ~/spammer/spammer.py $PREFIX/bin/spammer
-	dos2unix $PREFIX/bin/spammer
-	chmod +x $PREFIX/bin/spammer
-	spammer
-elif [ -d "/usr/bin/" ]
-then
-	if [ "$(whoami)" != 'root' ];
-                then
-                        echo "Запустите install.sh с root правами"
-                        exit
-                else
-                        apt-get update upgrade -y
-        		apt-get install python3 python3-pip dos2unix -y
-        		pip3 install -r requirements.txt
-        		cp ~/spammer/spammer.py /usr/local/bin/spammer
-        		dos2unix /usr/local/bin/spammer
-        		chmod +x /usr/local/bin/spammer
-        		spammer
-               fi
+
+if [ -e  /usr/lib/sudo ];then
+  if [ -e /usr/bin/apt-get ];then
+    sudo apt-get update
+    sudo apt-get upgrade -y
+    sudo apt-get install python3 -y
+    sudo cp ~/spammer/spammer.py /usr/bin/spammer
+    chmod +x /usr/bin/spammer
+  fi
+else
+  if [ -d /usr/bin ];then
+    if [ -e /usr/bin/apt-get ];then
+      apt-get update
+      apt-get upgrade -y
+      apt-get install python3 -y
+      cp ~/spammer/spammer.py /usr/bin/spammer
+      chmod +x /usr/bin/spammer
+    fi
+  fi
 fi
+
+if [ -d /data/data/com.termux/files/usr/bin ]; then
+  pkg update
+  pkg upgrade -y
+  pkg install python3 -y
+  cp ~/spammer/spammer.py /data/data/com.termux/files/usr/bin/spammer
+	chmod +x /data/data/com.termux/files/usr/bin/spammer
+fi
+
+if [ -e  /usr/lib/sudo ];then
+  if [ -e /usr/bin/yum ];then
+    sudo yum update
+    sudo yum upgrade -y
+    sudo yum install python3 -y
+    sudo cp ~/spammer/spammer.py /usr/bin/spammer
+    chmod +x /usr/bin/spammer
+  fi
+else
+  if [ -d /usr/bin ];then
+    if [ -e /usr/bin/yum ];then
+      yum update
+      yum upgrade -y
+      yum install python3 -y
+      cp ~/spammer/spammer.py /usr/bin/spammer
+      chmod +x /usr/bin/spammer
+    fi
+  fi
+fi
+
+if [ -e  /usr/local/bin/brew ];then
+  brew install python3 -y
+  cp ~/spammer/spammer.py /usr/local/bin/spammer
+  chmod +x /usr/local/bin/spammer
+fi
+
+if [ -e  /usr/bin/apk ];then
+  apk install python3 -y
+  cp ~/spammer/spammer.py /usr/bin/spammer
+  chmod +x /usr/bin/spammer
+fi
+
+pip install -r ~/spammer/requirements.txt
+spammer
